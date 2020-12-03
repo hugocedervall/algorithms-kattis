c090849e3aed7c213b22a1036acb607246d313d6
Hugo Cedervall <hugce564@student.liu.se>
2020-12-03 17:54:49 +0100

    Sumset kattis solution

--- sumset/main.cpp
+++ sumset/main.cpp
@@ -2,72 +2,71 @@
 #include <set>
 #include <algorithm>
 #include <vector>
-#include <map>
+#include <unordered_map>
 #include<tuple>
 
 using namespace std;
 
-
 tuple<int, bool> algo(const set<int> &numbers) {
-    // number -> pairs used to construct number
-    map<int, vector<tuple<int, int>>> doubleNumbers;
+    // number -> tuple used to construct number
+    unordered_map<int, vector<tuple<int, int>>> doubleNumbers;
 
-
+    // All possible permutations for 2 numbers of set
     for (auto it = numbers.begin(); it != numbers.end(); it++) {
         for (auto it2 = next(it); it2 != numbers.end(); it2++) {
             doubleNumbers[*it2 + *it].push_back(make_tuple(*it, *it2));
         }
     }
 
+    int min = *min_element(numbers.begin(), numbers.end());
 
-    int biggest = *min_element(numbers.begin(), numbers.end());
-    bool valid = false;
+    // Biggest found d
+    int biggest = min - 1;
 
     for (auto d : numbers) {
         for (auto c: numbers) {
             if (d == c) continue;
 
             if (doubleNumbers.find(d - c) != doubleNumbers.end()) {
+
+                // See if there is a tuple not containing a or b that constructs d - c
                 bool found = false;
-                // Check that c or d has not been used in composition of a+b+c=d before.
                 for (tuple<int, int> nums : doubleNumbers[d - c]) {
-                    if (get<0>(nums) == c || get<1>(nums) == c || get<0>(nums) == d || get<1>(nums) == d) {
+                    int a = get<0>(nums);
+                    int b = get<1>(nums);
+                    if (a != c && b != c && a != d && b != d) {
                         found = true;
                         break;
                     }
                 }
-                if (!found && d >= biggest) {
+
+                if (found && d > biggest) {
                     biggest = d;
-                    valid = true;
-                    //break;
+                    break;
                 }
             }
-
         }
-
     }
 
-    return make_tuple(biggest, valid);
+    return make_tuple(biggest, (biggest > min - 1));
 }
 
 
 int main() {
-
-
     int N;
-    while(cin >> N && N != 0) {
+    cin >> N;
 
-        set<int> numbers;
-        for (int i = 0; i < N; i++) {
-            int temp;
-            cin >> temp;
-            numbers.insert(temp);
-        }
-
-        tuple<int, bool> results = algo(numbers);
-
-        if (!get<1>(results)) cout << "no solution" << endl;
-        else cout << get<0>(results) << endl;
+    set<int> numbers;
+    for (int i = 0; i < N; i++) {
+        int temp;
+        cin >> temp;
+        numbers.insert(temp);
     }
+
+    tuple<int, bool> results = algo(numbers);
+
+    if (!get<1>(results)) cout << "no solution" << endl;
+    else cout << get<0>(results) << endl;
+
     return 0;
 }
