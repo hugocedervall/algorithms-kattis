e1fbbd8f6930937fe1a39ebf93a0b4ebd5de8366
Hugo Cedervall <hugce564@student.liu.se>
2021-04-12 10:10:35 +0200

    modulararithmetic solution

---
+++ modulararithmetic/main.cpp
@@ -0,0 +1,135 @@
+/*
+ * @AUTHOR: Hugo Cedervall
+ * @DATE: 2021-04-12
+ * @PROBLEM: modulararithmetic
+ *      The problem is to implement modular arithmetic
+ *      operations between two numbers.
+ *
+ * @COMPLEXITIES:
+ *      TIME: O(1) for add, subtract, multiply. O(n) for division.
+ *
+*/
+
+#include <bits/stdc++.h>
+#include <iostream>
+
+
+using namespace std;
+
+typedef long l;
+
+/**
+ * Calculated the modulus operation of a % b.
+ * Also handles negative numbers.
+ */
+l mod(l a, l b) {
+    return (b + (a % b)) % b;
+}
+
+/**
+ *
+ * @param a
+ * @param b
+ * @param x Where result of first coefficient will be put
+ * @param y Where result of second coefficient will be put
+ * @return
+ */
+l extendedEucliden(l a, l b, l *x, l *y) {
+    if (b == 0) {
+        *x = 1, *y = 0;
+        return a;
+    }
+    l x1, y1;
+    l gcd = extendedEucliden(b, a % b, &x1, &y1);
+
+    *x = y1;
+    *y = x1 - y1 * (a / b);
+    return gcd;
+}
+
+/**
+ * Calculates x such that a*x = 1 (mod m). In other words,
+ * calculate the modular multiplicative inverse x of a such that
+ * the product a*x i congurent to 1 modulo m.
+ *
+ * @param a The number to calculate the corresponding multiplicative
+ * inverse of.
+ * @param m modulus
+ * @return the modulo inverse of @param a
+ */
+l modInv(l a, l m) {
+    l x, y;
+    l reduction = extendedEucliden(a, m, &x, &y);
+    // No solution if reduction isn't 1
+    if (reduction != 1)
+        return -1;
+    return mod(x, m);
+}
+
+/**
+ * Calculates (a+b)%m
+ */
+l addMod(l a, l b, l m) {
+    return mod(a + b, m);
+}
+
+/**
+ * Calculates (a-b)%m
+ */
+l subtractMod(l a, l b, l m) {
+    return mod(a - b, m);
+}
+
+/**
+ * Calculates (a*b)%m
+ */
+l multiplyMod(l a, l b, l m) {
+    return mod(a * b, m);
+}
+
+/**
+ * Calculates (a/b)%m. This is done through
+ */
+l divideMod(l a, l b, l m) {
+    l inv = modInv(b, m);
+    if (inv != -1)
+        return multiplyMod(a, inv, m);
+    return -1;
+}
+
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    l n, t, a, b;
+    char op;
+    while (cin >> n && cin >> t && (n != 0 && t != 0)) {
+        while (t--) {
+            cin >> a >> op >> b;
+
+            l res = -1;
+
+            switch (op) {
+                case '+':
+                    res = addMod(a, b, n);
+                    break;
+                case '-':
+                    res = subtractMod(a, b, n);
+                    break;
+                case '/':
+                    res = divideMod(a, b, n);
+                    break;
+                case '*':
+                    res = multiplyMod(a, b, n);
+                    break;
+                default:
+                    break;
+            }
+            cout << res << '\n';
+        }
+    }
+    cout << flush;
+
+    return 0;
+}
